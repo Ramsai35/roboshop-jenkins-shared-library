03121eef91dbebe9c98ec69f66ed39a360aa2ddf
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-02 04:53:41 +0530

    unit test

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -23,13 +23,18 @@ def call() {
         }
 
         stage('Quality Control') {
-            steps
-            script {
-                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                {sh 'sonar-scanner -Dsonar.host.url=http://172.31.5.53:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart'
+            environment {
+                SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
             }
+            steps {
+                script {
+                    SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.5.53:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                    }
                 }
+            }
         }
 
         stage('Upload Code Into S3 bucket'){
