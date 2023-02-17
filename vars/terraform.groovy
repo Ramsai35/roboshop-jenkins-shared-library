15e911bb2ee866c5cc90eb3940d2c6cc50ff061b
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-17 17:45:59 +0530

    Vpc creation

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -22,13 +22,19 @@ def call() {
                 }
             }
 
-//            stage('Terraform Apply') {
-//                steps {
-//                    sh "terraform apply -auto-approve  -var-file=env-${INFRA_ENV}/state.tfvars"
-//                }
-//            }
+            stage('Terraform Apply') {
+                steps {
+                    sh "terraform apply -auto-approve  -var-file=env-${INFRA_ENV}/state.tfvars"
+                }
+            }
 
 
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
         }
 
     }
