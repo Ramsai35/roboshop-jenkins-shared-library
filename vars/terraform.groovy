7919463d87a6c79550baa2b8beabde4f3be3921a
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-17 16:32:13 +0530

    create Infra jobs

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -22,11 +22,11 @@ def call() {
                 }
             }
 
-            stage('Terraform Apply') {
-                steps {
-                    sh "terraform apply -auto-approve  -var-file=env-${INFRA_ENV}/state.tfvars"
-                }
-            }
+//            stage('Terraform Apply') {
+//                steps {
+//                    sh "terraform apply -auto-approve  -var-file=env-${INFRA_ENV}/state.tfvars"
+//                }
+//            }
 
 
         }
