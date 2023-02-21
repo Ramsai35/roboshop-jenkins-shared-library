a3b1d08dcabef9c85eea9443b7dcb03e3f26471f
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-21 08:19:20 +0530

    route

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -12,7 +12,7 @@ def call() {
 
         parameters {
             string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter ENV like dev or prod')
-            choice(name: 'ACTION', choices: ['apply', 'destroy'], description: 'Action')
+            choice(name: 'ACTION', choices: ['apply' , 'destroy'], description: 'Action')
         }
 
         stages {
